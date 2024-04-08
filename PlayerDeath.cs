6c8415999370b4a25cb102f2742b9484a1584463
Om-Padhiyar <123418018+Om-Padhiyar@users.noreply.github.com>
2024-04-08 18:13:38 -0400

    Add files via upload
    I set up a health system with 3 hearts, losing a heart every time it hit an obstacle or enemy, and losing all hearts if something instantly kills it.

--- PlayerDeath.cs
+++ PlayerDeath.cs
@@ -7,17 +7,23 @@ public class PlayerDeath : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody2D rb;
+    public int value;
+
+    public PlayerHearts ph;
     // Start is called before the first frame update
     void Start()
     {
+        value =0;
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        ph = GetComponent<PlayerHearts>();
     }
+
 
     // Update is called once per frame
     private void OnCollisionEnter2D(Collision2D collision)
     {
-      if (collision.gameObject.CompareTag("death")){
+      if (collision.gameObject.CompareTag("instantdeath")){
        Die(); 
   //Destroy(gameObject);
       }
@@ -27,6 +33,9 @@ public class PlayerDeath : MonoBehaviour
     {
         rb.bodyType = RigidbodyType2D.Static;
         anim.SetTrigger("Death");
+
+      ph.SetHealth(value);
+
     }
     private void RestartLevel(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
